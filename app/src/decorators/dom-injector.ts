1f7b05bba7a7f1cb3b100b3141de63c0b4bbf917
Ially Fabiane <iallyfabiane@gmail.com>
2022-05-26 14:36:32 -0300

    aplicando decorator de propriedade em negociacoes-controller

--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,8 +1,19 @@
-export function domInjector(seletor: string) {
+export function domInjector(seletor: string) { //decorator de captura de elementos do DOM
      return function (
         target: any,
-        propertyKey: string // atributo no qual o decorator foi colocado
+        propertyKey: string // propriedade na qual o decorator foi colocado
     ) {
+        console.log(`modificando prototype ${target.constructor.name} e adicionando getter para a propriedade ${propertyKey}`);
+        const getter = function() { // função que busca o elemento do DOM e o retorna
+            const elemento = document.querySelector(seletor);
+            console.log(`buscando o  elemento do DOM com o seletor${seletor} para injetar em ${propertyKey}`);
+            return elemento;
+        }
 
+        Object.defineProperty( // captura o prototype9com o target), aplica na propriedade (propertyKey) e define o getter (get)
+            target, 
+            propertyKey,
+            { get: getter }
+        );
     }
 }
