d65c149e6695cb69bdd5988fa4f268d45b2c9049
Ially Fabiane <iallyfabiane@gmail.com>
2022-05-27 10:18:17 -0300

    conectando o servidor da api

--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -4,10 +4,14 @@ export function domInjector(seletor: string) { //decorator de captura de element
         propertyKey: string // propriedade na qual o decorator foi colocado
     ) {
         console.log(`modificando prototype ${target.constructor.name} e adicionando getter para a propriedade ${propertyKey}`);
+        let elemento: HTMLElement;
         const getter = function() { // função que busca o elemento do DOM e o retorna
-            const elemento = document.querySelector(seletor);
-            console.log(`buscando o  elemento do DOM com o seletor${seletor} para injetar em ${propertyKey}`);
-            return elemento;
+           if(!elemento) { // se não existir elemento, ele é capturado e guardado dentro da variável elemento
+               elemento = <HTMLElement>document.querySelector(seletor);
+               console.log(`buscando o  elemento do DOM com o seletor${seletor} para injetar em ${propertyKey}`);
+           }
+            
+            return elemento; // se elemento já foi capturado, ele é retornado
         }
 
         Object.defineProperty( // captura o prototype9com o target), aplica na propriedade (propertyKey) e define o getter (get)
