eaa99038cf58cff57e88a87f505e1643255a653f
Ially Fabiane <iallyfabiane@gmail.com>
2022-05-27 10:33:00 -0300

    criando método importaDados em negociacao-controller

--- app/src/app.ts
+++ app/src/app.ts
@@ -10,3 +10,12 @@ if (form) {
 } else {
     throw Error('Não foi possível inicializar a aplicação. Verifique se o form existe.');
 }
+
+const botaoImporta = document.querySelector('#botao-importa'); //capturando o botao-importa com o querySelector
+if (botaoImporta) {
+    botaoImporta.addEventListener('click', () => { //adicionando o evento de clicar para acionar a execução do método importaDados de negociacao-controller.ts
+        controller.importaDados(); //chamando o método que está em negociacao-controller
+    })
+} else {
+    throw Error('Botão importa não foi encontrado');
+}
